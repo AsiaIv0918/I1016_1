b30bf21762527c9c64a9cb0cb05579c7bd66b6e5
AsiaIV0918 <104051067@live.asia.edu.tw>
2017-10-16 15:02:25 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+		String x = in.next();
+		int a= Integer.valueOf(x,16);
+		System.out.println((char)a);
+	}
+}
