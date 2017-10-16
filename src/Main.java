b066b3901c3d8cd7685b6ba54cdfb8a4098c086f
AsiaIV0918 <104051067@live.asia.edu.tw>
2017-10-16 15:05:47 +0800

    補充

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.util.*;
+//int 轉萬國碼(char)
 public class Main {
     public static void main(String[] args) {
     Scanner in=new Scanner(System.in);
 		String x = in.next();
-		int a= Integer.valueOf(x,16);
-		System.out.println((char)a);
+		int a= Integer.valueOf(x,16); //轉16進位
+		System.out.println((char)a);         //轉字
 	}
 }
